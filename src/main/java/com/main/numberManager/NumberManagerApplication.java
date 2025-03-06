f8d4018573dbba72205d29ca113dfd593471a8bd
Lucas Costa <lukascosta.ls17@gmail.com>
2025-03-06 09:00:45 -0300

    Ajuste na importação

--- src/main/java/com/main/numberManager/NumberManagerApplication.java
+++ src/main/java/com/main/numberManager/NumberManagerApplication.java
@@ -1,6 +1,5 @@
 package com.main.numberManager;
 
-import com.main.numberManager.model.basesCnl.CnlUltracom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
