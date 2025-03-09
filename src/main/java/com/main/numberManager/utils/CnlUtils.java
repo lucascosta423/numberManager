4943e7618b690692ba6990792dec3bf3429993d1
Lucas <lukascosta.ls17@gmail.com>
2025-03-09 00:17:11 -0300

    Fix: Ajuste nos metodos readFile e mapToModel. readFile: Foi ajustados a troca do uso list para Stream. mapToModel: Foi refatorado todo o metodo, onde teve ajustes para uma melhor performance.

--- src/main/java/com/main/numberManager/utils/CnlUtils.java
+++ src/main/java/com/main/numberManager/utils/CnlUtils.java
@@ -1,22 +1,23 @@
 package com.main.numberManager.utils;
 
-import org.springframework.beans.BeanUtils;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CnlUtils{
 
-    public static List<String> readerFile(MultipartFile file) throws IOException {
-        return new BufferedReader(new InputStreamReader(file.getInputStream()))
-                .lines()
-                .toList();
+    public static Stream<String> readerFile(MultipartFile file) throws IOException {
+        return new BufferedReader(new InputStreamReader(file.getInputStream())).lines();
     }
 
     public static <T> List<T> parseLines(List<String> lines, Function<String, T> mapper) {
@@ -35,11 +36,32 @@ public class CnlUtils{
         try {
             String[] parts = line.split(";");
 
-            if (parts.length < 13) { // Ajuste conforme necessário para outros modelos
+            if (parts.length < 13) { // Ajuste conforme o número de colunas esperado
                 throw new IllegalArgumentException("Linha inválida: " + line + " (Esperado >= 13 colunas, encontrado " + parts.length + ")");
             }
+
             T model = type.getDeclaredConstructor().newInstance();
-            BeanUtils.copyProperties(parts, model);
+
+            Field[] fields = type.getDeclaredFields();
+            int partIndex = 0; // Índice para percorrer os dados do arquivo
+
+            for (Field field : fields) {
+                field.setAccessible(true);
+
+                // Ignorar campos anotados como @Id ou @GeneratedValue
+                if (field.isAnnotationPresent(Id.class) || field.isAnnotationPresent(GeneratedValue.class)) {
+                    continue;
+                }
+
+                if (partIndex < parts.length) {
+                    String value = parts[partIndex].trim();
+                    if (!value.isEmpty()) {
+                        field.set(model, value); // Atribui valores aos campos do modelo
+                    }
+                }
+                partIndex++;
+            }
+
             return model;
         } catch (Exception e) {
             throw new RuntimeException("Erro ao mapear linha para " + type.getSimpleName(), e);
