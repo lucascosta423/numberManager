94126f2de5a8aa9428954a957164796ffd5925eb
lucas <lukascosta.ls17@gmail.com>
2025-04-28 23:42:57 -0300

    refactor: ajustando o tipo do id no repository

--- src/main/java/com/main/numberManager/repositorys/RequestPortabilityRepository.java
+++ src/main/java/com/main/numberManager/repositorys/RequestPortabilityRepository.java
@@ -3,6 +3,6 @@ package com.main.numberManager.repositorys;
 import com.main.numberManager.models.RequestPortabilityModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RequestPortabilityRepository extends JpaRepository<RequestPortabilityModel, Integer> {
+public interface RequestPortabilityRepository extends JpaRepository<RequestPortabilityModel, String> {
 
 }
