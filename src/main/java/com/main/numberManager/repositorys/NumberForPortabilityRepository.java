b024de02dfe97cbbf0f1c8c25ce32692be6eef94
lucas <lukascosta.ls17@gmail.com>
2025-04-28 23:23:57 -0300

    Refactor: removendo um import sem uso

--- src/main/java/com/main/numberManager/repositorys/NumberForPortabilityRepository.java
+++ src/main/java/com/main/numberManager/repositorys/NumberForPortabilityRepository.java
@@ -4,7 +4,6 @@ import com.main.numberManager.models.NumberForPortabilityModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
 
 @Repository
 public interface NumberForPortabilityRepository extends JpaRepository<NumberForPortabilityModel, String> {
