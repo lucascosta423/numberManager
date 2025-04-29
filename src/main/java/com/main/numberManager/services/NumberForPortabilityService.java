be9fd61951800f44b069c2014d17906bb8a216d0
lucas <lukascosta.ls17@gmail.com>
2025-04-28 23:24:33 -0300

    Refactor: imports para serem utilizados no service

--- src/main/java/com/main/numberManager/services/NumberForPortabilityService.java
+++ src/main/java/com/main/numberManager/services/NumberForPortabilityService.java
@@ -1,12 +1,19 @@
 package com.main.numberManager.services;
 
+import com.main.numberManager.Enuns.StatusPortability;
+import com.main.numberManager.dtos.Portability.UpdateNumberForPortabilityDTO;
+import com.main.numberManager.exeptions.NotFoundException;
 import com.main.numberManager.models.NumberForPortabilityModel;
+import com.main.numberManager.models.OperatorsModel;
 import com.main.numberManager.models.RequestPortabilityModel;
 import com.main.numberManager.repositorys.NumberForPortabilityRepository;
+import com.main.numberManager.utils.responseApi.SucessResponse;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class NumberForPortabilityService {
